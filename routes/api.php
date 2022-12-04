3db97d88d593b1abc7bf3906b3192df7638db9e0
Hayyunulinuha123 <3103120107@student.smktelkom-pwt.sch.id>
2022-12-04 19:03:00 +0700

    Rental Mobil

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MobilController;
+use App\Http\Controllers\PesananController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/mobil', [MobilController::class, 'index']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('/booking', PesananController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    //Admin
+    Route::middleware('admin')->group(function () {
+        Route::resource('/mobils', MobilController::class)->except('create', 'edit', 'show', 'index');
+        Route::get('/mobil/{id}', [MobilController::class, 'show']);
+    });
+});
